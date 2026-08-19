5ac0961fa73c36c54dd521f4bd84e74907d379ec
truenickspivak <nickspivakk@gmail.com>
2026-08-19 18:41:28 -0400

    Initial commit with blinking LED code on the Feather

---
+++ firmware.py
@@ -0,0 +1,13 @@
+import board
+import digitalio
+import time
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    print("Hello!")
+    led.value = True
+    time.sleep(0.1)
+    led.value = False
+    time.sleep(0.1)
